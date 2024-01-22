357983b07f2df654315a838e544d74fe48b11a10
Deoships <deoships@mail.ru>
2024-01-22 16:07:04 +0300

    about +

---
+++ pages/contacts.php
@@ -0,0 +1,68 @@
+
+<?php
+include '../includes/header.php';
+?>
+
+<section>
+    <div class="block">
+        <div class="content">
+            <h1 class="contact-h1">Более 10 лет на рынке</h1>
+        <div class="contact-desc">
+            <div class="contact-item">
+            <p class="contact-p"><b>15млн +</b><br> клиентов</p>
+        </div>
+            <div class="contact-item">
+            <p class="contact-p"><b>5</b><br>средняя оценка</p>
+        </div>
+            <div class="contact-item">
+            <p class="contact-p"><b><1%</b><br> Браков</p>
+        </div>
+        </div>
+            <div class="content-img"></div>
+        </div>
+    </div>
+
+    <div class="block">
+        <h1 class="contact-h1">Быстро доставим любой Ваш заказ в любую точку Беларуси</h1>
+        <div class="delivery">
+            <div class="item">
+                <img src="../img/Truck.png" alt="Truck">
+                <p  class="contact-p">Бесплатная доставка</p>
+            </div>
+            <div class="item">
+                <img src="../img/History.png" alt="History">
+                <p  class="contact-p">Доставка в срок</p>
+            </div>
+            <div class="item">
+                <img src="../img/box_open.png" alt="box_open">
+                <p  class="contact-p">Гарантия качества</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="block">
+        <div class="contact-info">
+        <img src="../img/present.png" alt="present">
+        <div class="contact-text">
+        <h2 class="contact-h2">Наша история</h2>
+        <p class="contact-p2">В момент своего зарождения как стартап в эпоху технологической революции, наша компания выросла из маленького магазина в мирового лидера электронной индустрии. Символ передовых технологий, мы не только предоставляем инновационные продукты, но и вдохновляем мировое сообщество к поиску будущего через надежность, качество и преданность технологическому совершенству.</p>
+        <p class="contact-p2">С нашей командой сверхлюдей и глобальным воздействием, мы превратились в неотъемлемую часть повседневной жизни людей, переписав историю человечества и оставляя свой след в области науки, медицины и техники.</p>
+        </div> 
+    </div>
+</div>
+<div class="block">
+        <div class="contact-info">
+        <img src="../img/delivery.png" alt="delivery">
+        <div class="contact-text">
+        <h2 class="contact-h2">Кратко о сотрудниках</h2>
+        <p class="contact-p2">В нашей компании работают истинные технологические гении, каждый из которых является сверхчеловеком в своей области. Наши специалисты по продажам обладают уникальным интеллектом и невероятной интуицией в предсказании потребностей клиентов. Они могут с легкостью анализировать самые сложные технические характеристики и предлагать идеальные решения для каждого клиента.</p>
+        <h2 class="contact-h2">ВНИМАНИЕ!</h2>
+        <p class="contact-p2">Наш отдел логистики функционирует как надежный механизм, позволяя нам обеспечивать оперативную доставку в любую точку мира. Эти люди обладают способностью управлять процессами с беспрецедентной эффективностью, что делает наш сервис несравненным в мире электроники.</p>
+        </div> 
+    </div>
+</section>
+<?php
+include '../includes/footer.php';
+?>
+</body>
+</html>

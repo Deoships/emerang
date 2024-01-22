3049b18fc20d6afb7a822a628c6b281c4b1ef7bf
Deoships <deoships@mail.ru>
2024-01-22 12:21:28 +0300

    contacts +

--- pages/about.php
+++ pages/about.php
@@ -1,33 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../img/logo_ico.png" type="image/x-icon">
-    <link rel="stylesheet" href="../scss/about.css">
-    <title>О нас</title>
-</head>
-<body>
+
 <?php
 include '../includes/header.php';
 ?>
+
+<section>
  <div class="about-container">
         <div class="contact-block">
-            <h2>Контакты</h2>
+            <h2 class="contact-h2">Контакты</h2>
             <div class="line"></div>
-                <ul class="contact-flex">
-                    <li><img src="../img/mail.png" alt="адрес"><p>Беларусь, г. Брест, ул. Московская, 123</p></li>
-                    <li><img src="../img/mail.png" alt="телефон"><p>+7 123 456 78 90</p></li>
-                    <li><img src="../img/mail.png" alt="почта"><p>market@mail.ru</p></li>
-                    <li><img src="../img/mail.png" alt="расписание"><p>Время работы: с 8:00 до 20:00</p></li>
+                <ul class="contact-ul">
+                    <li class="contact-ul-li"><img class="contact-ul-li-img" src="../img/place.png" alt="адрес"><p class="contact-ul-li-img-p">Беларусь, г. Брест, ул. Московская, 123</p></li>
+                    <li class="contact-ul-li"><img class="contact-ul-li-img" src="../img/telephone.png" alt="телефон"><p class="contact-ul-li-img-p">+7 123 456 78 90</p></li>
+                    <li class="contact-ul-li"><img class="contact-ul-li-img" src="../img/mail.png" alt="почта"><p class="contact-ul-li-img-p">market@mail.ru</p></li>
+                    <li class="contact-ul-li"><img class="contact-ul-li-img" src="../img/time.png" alt="расписание"><p class="contact-ul-li-img-p">Время работы:  8:00-20:00</p></li>
                  </ul>
         </div>
         <div class="location-block">
-            <h2>Наше расположение</h2>
+            <h2 class="contact-h2">Наше расположение</h2>
             <div class="line-bottom"></div>
-            <div class="map"></div>
+            <div class="map">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A677f078ec2b5de1770114f6c625d76767741174a4e61762433371cbb60c36605&amp;width=770&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
         </div>
     </div>
+</section>
 <?php
 include '../includes/footer.php';
 ?>

3a6f153792d7903e0eb7bfb3764f5ce771c00f71
Deoships <deoships@mail.ru>
2024-05-04 01:04:59 +0300

    registr form scss

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@ include '../includes/header.php';
 
 <section class="auth">
 <div class="auth-container">
-        <h3>Авторизация</h3>
+        <h3 class="form-h3">Авторизация</h3>
         <form action="process_login.php" method="post">
             <div class="form-group">
                 <input type="text" name="login" placeholder="Логин">
@@ -13,8 +13,8 @@ include '../includes/header.php';
                 <input type="password" name="password" placeholder="Пароль">
             </div>
             <div class="btn-container">
-                <button type="submit">Зарегистрироваться</button>
-                <button type="submit">Войти</button>
+                <button class="button-log" type="submit">Зарегистрироваться</button>
+                <button class="button" type="submit">Войти</button>
             </div>
         </form>
     </div>
--- pages/registr.php
+++ pages/registr.php
@@ -2,7 +2,55 @@
 include '../includes/header.php';
 ?>
 
+<section class="reg">
+    <div class="reg-container">
+        <h3 class="form-h3">Регистрация</h3>
+        <form action="process_registration.php" method="post" id="registrationForm">
+            <div class="form-group">
+                <input type="text" name="first_name" placeholder="Имя">
+            </div>
+            <div class="form-group">
+                <input type="text" name="last_name" placeholder="Фамилия">
+            </div>
+            <div class="form-group">
+                <input type="tel" name="phone" id="phone" placeholder="Телефон">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email" required>
+            </div>
+            <div class="form-group">
+                <input type="text" name="login" placeholder="Логин">
+            </div>
+            <div class="form-group password-container">
+                <input type="password" name="password" id="password" class="password-field" placeholder="Пароль">
+                <span onclick="showPassword('password')" class="eye-icon"> </span> <!-- Глазик для показа пароля -->
+            </div>
+            <div class="form-group password-container">
+                <input type="password" name="confirm_password" id="confirm_password" class="password-field" placeholder="Повторите пароль">
+                <span onclick="showPassword('confirm_password')" class="eye-icon"> </span> <!-- Глазик для показа пароля -->
+            </div>
+            <div class="btn-container">
+                <button class="button-reg" type="submit">Авторизоваться</button>
+                <button class="button" type="submit">Зарегестрироваться</button>
+            </div>
+        </form>
+    </div>
+</section>
 
+<script>
+    $(document).ready(function(){
+        $('#phone').mask('+7 (999) 999-99-99');
+    });
+
+    function showPassword(inputId) {
+        var passwordField = document.getElementById(inputId);
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+        } else {
+            passwordField.type = "password";
+        }
+    }
+</script>
 
 <?php
 include '../includes/footer.php';
